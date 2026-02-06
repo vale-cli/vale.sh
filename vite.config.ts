e176649dfbfc160b3ca1702cccc570b70192a1fc
Joseph Kato <joseph@jdkato.io>
2026-02-05 20:00:24 -0800

    fix generator
    Signed-off-by: Joseph Kato <joseph@jdkato.io>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    optimizeDeps: {
-        exclude: [
-            "svelte-codemirror-editor",
-            "codemirror",
-            "@codemirror/lang-yaml",
-            "@codemirror/language",
-            "@codemirror/view",
-            "@codemirror/state",
-            "@codemirror/legacy-modes",
-            "@codemirror/theme-one-dark",
-        ],
-    },
+    resolve: {
+        alias: {
+            // Force all instances of CodeMirror core to use the same physical file
+            '@codemirror/state': path.resolve('./node_modules/@codemirror/state'),
+            '@codemirror/view': path.resolve('./node_modules/@codemirror/view'),
+            '@codemirror/language': path.resolve('./node_modules/@codemirror/language'),
+        }
+    }
 });
